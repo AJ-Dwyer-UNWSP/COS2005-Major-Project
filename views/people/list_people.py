f42ff3b05f8e811e63e351740fdc8b2c107730f1
AJ-Dwyer-UNWSP <ajdwyer@students.unwsp.edu>
2022-04-29 15:12:44 -0500

    updated people screens

--- views/people/list_people.py
+++ views/people/list_people.py
@@ -4,39 +4,81 @@
 import tkinter
 import tkinter.messagebox
 from data_context import DataContext
-from data_models.person import Person
 from views.people.new_edit_people import NewEditPeopleView
+import tkinter.messagebox
 
 
 class ListPeopleView:
     def __init__(self, data_context: DataContext):
         # set up fields
         self.__data_context = data_context
-        self.__curr_person: Person = Person(1, "1", "1")
         self.__lst_people = self.__data_context.get_people()
-        print(self.__lst_people)
+        self.__curr_person = None
 
         # set up view
         self.__main_window = tkinter.Toplevel()
         self.__main_window.title("People")
-        self.__edit_btn = tkinter.Button(self.__main_window, text="Edit", command=self.__edit_person)
+
+        # setup frames
+        self.__list_people_frame = tkinter.Frame(self.__main_window)
+        self.__btns_frame = tkinter.Frame(self.__main_window)
+
+        # set up list view
+        self.__list_people_var = tkinter.StringVar()
+        self.__list_people_var.set(self.__lst_people)
+        self.__list_people_box = tkinter.Listbox(self.__list_people_frame, listvariable=self.__list_people_var)
+        # bind to the event that an item is clicked in the listbox
+        self.__list_people_box.bind("<<ListboxSelect>>", self.__on_listbox_item_click)
+        # create the scrollbar
+        self.__scroll_bar = tkinter.Scrollbar(self.__list_people_frame, orient=tkinter.VERTICAL,
+                                              command=self.__list_people_box.yview)
+        # set the listbox to set the scroll bar when it moves
+        self.__list_people_box.config(yscrollcommand=self.__scroll_bar.set)
+
+        # set up btns
+        self.__new_btn = tkinter.Button(self.__btns_frame, text="New", command=self.__new_person)
+        self.__edit_btn = tkinter.Button(self.__btns_frame, text="Edit", command=self.__edit_person)
+        self.__delete_btn = tkinter.Button(self.__btns_frame, text="Delete", command=self.__delete_curr_person)
+        self.__cancel_btn = tkinter.Button(self.__btns_frame, text="Cancel", command=self.__main_window.destroy)
+
+        # pack elements
+        self.__list_people_box.pack(side="left")
+        self.__scroll_bar.pack(side="right", fill=tkinter.Y)
+        self.__new_btn.pack(side="left")
         self.__edit_btn.pack(side="left")
+        self.__delete_btn.pack(side="left")
+        self.__cancel_btn.pack(side="left")
+
+        self.__list_people_frame.pack(padx=10, pady=10)
+        self.__btns_frame.pack(padx=10, pady=10)
+
+    def __on_listbox_item_click(self, event):
+        index = self.__list_people_box.curselection()[0]
+        self.__curr_person = self.__lst_people[index]
 
     def __new_person(self):
         # open the new person view and set is_edit_view to false and person to None
-        NewEditPeopleView(self.__data_context, False, None)
+        NewEditPeopleView(self.__data_context, False, None, self.__update_data)
 
     def __edit_person(self):
         # open the edit view if the current person is not None to avoid an Exception
         if self.__curr_person is not None:
-            NewEditPeopleView(self.__data_context, True, self.__curr_person)
+            NewEditPeopleView(self.__data_context, True, self.__curr_person, self.__update_data)
+        else:
+            tkinter.messagebox.showinfo("Error", "Please select an item before performing your operation.")
 
     def __delete_curr_person(self):
         # delete the current person if it is not None
         if self.__curr_person is not None:
             # delete the person and show a message if the operation failed
-            if not self.__data_context.delete_person(self.__curr_person.get_person_id()):
+            if not self.__data_context.delete_person(self.__curr_person):
                 self.__display_error()
+        else:
+            tkinter.messagebox.showinfo("Error", "Please select an item before performing your operation.")
 
     def __display_error(self):
-        tkinter.messagebox.showinfo("There was an error. Please try again.")
+        tkinter.messagebox.showinfo("Error", "There was an error. Please try again.")
+
+    def __update_data(self):
+        self.__lst_people = self.__data_context.get_people()
+        self.__list_people_var.set(self.__lst_people)
--- views/people/new_edit_people.py
+++ views/people/new_edit_people.py
@@ -5,40 +5,88 @@ import tkinter
 from data_context import DataContext
 from data_models.person import Person
 import random
+import tkinter.messagebox
 
 
 class NewEditPeopleView:
     # is_edit_view tells whether the view is used as an edit or a create view
+    # on_data_updated is a callback to tell the previous screen to update its data
     # person is the person to edit; it is only needed if used as an edit view
-    def __init__(self, data_context: DataContext, is_edit_view, person):
+    def __init__(self, data_context: DataContext, is_edit_view, person, on_data_updated):
         # set up data fields
         self.__data_context = data_context
         self.__is_edit_view = is_edit_view
+        self.__on_data_updated = on_data_updated
         self.__cur_person = person
-        self.__first_name = ''
-        self.__last_name = ''
-        print(self.__cur_person)
+        # set __first_name and __last_name
+        if is_edit_view:  # then set to the person values passed in
+            self.__first_name = person.get_first_name()
+            self.__last_name = person.get_last_name()
+        else:  # then set to default view
+            self.__first_name = ''
+            self.__last_name = ''
 
         # set up window
         self.__main_window = tkinter.Toplevel()
         self.__main_window.title(f"{'Edit' if is_edit_view else 'New'} Person")
 
+        # set up frames
+        self.__first_name_frame = tkinter.Frame(self.__main_window)
+        self.__last_name_frame = tkinter.Frame(self.__main_window)
+        self.__btn_frame = tkinter.Frame(self.__main_window)
+
+        # set up first name frame
+        self.__first_name_lbl = tkinter.Label(self.__first_name_frame, text="First Name")
+        self.__first_name_entry = tkinter.Entry(self.__first_name_frame)
+
+        # set up last name frame
+        self.__last_name_lbl = tkinter.Label(self.__last_name_frame, text="Last Name")
+        self.__last_name_entry = tkinter.Entry(self.__last_name_frame)
+
+        # set up bottom btns
+        self.__save_btn = tkinter.Button(self.__btn_frame, text="Save", command=self.__save)
+        self.__cancel_btn = tkinter.Button(self.__btn_frame, text="Cancel", command=self.__main_window.destroy)
+
+        # pack the elements
+        self.__first_name_lbl.pack(side="left")
+        self.__first_name_entry.pack(side="left")
+        self.__last_name_lbl.pack(side="left")
+        self.__last_name_entry.pack(side="left")
+        self.__save_btn.pack(side="left")
+        self.__cancel_btn.pack(side="left")
+        self.__first_name_frame.pack(padx=10, pady=10)
+        self.__last_name_frame.pack(padx=10, pady=10)
+        self.__btn_frame.pack(padx=10, pady=10, anchor="e")
+
+        # if is in edit mode, then set the value of the entries
+        if is_edit_view:
+            self.__first_name_entry.insert(0, self.__first_name)
+            self.__last_name_entry.insert(0, self.__last_name)
+
+        self.__main_window.mainloop()
+
     def __save(self):
+        # get the values from the entries
+        self.__first_name = self.__first_name_entry.get()
+        self.__last_name = self.__last_name_entry.get()
         # validate the inputs
         if self.__first_name != '' and self.__last_name != '':  # then the inputs are valid
-            # create an empty person for the user to fill out with a random id
-            self.__cur_person = Person(random.randint(0, 9999999), self.__first_name, self.__last_name)
-
             if self.__is_edit_view == True:  # then in edit mode and need to update person
+                self.__cur_person.set_last_name(self.__last_name)
+                self.__cur_person.set_first_name(self.__first_name)
                 # update and display a message if the operation failed
                 if not self.__data_context.update_person(self.__cur_person):
                     self.__display_error()
             else:  # then in create mode so need to add a new person
+                # create an empty person for the user to fill out with a random id
+                self.__cur_person = Person(random.randint(0, 9999999), self.__first_name, self.__last_name)
                 # create and display a message if the operation failed
                 if not self.__data_context.create_person(self.__cur_person):
                     self.__display_error()
+            self.__on_data_updated()  # call the callback for the previous screen to update its data
+            self.__main_window.destroy()
         else:  # then the inputs weren't valid
-            tkinter.messagebox.showinfo("All fields must be filled out to continue")
+            tkinter.messagebox.showinfo("Error", "All fields must be filled out to continue")
 
     def __display_error(self):
-        tkinter.messagebox.showinfo("There was an error. Please try again.")
+        tkinter.messagebox.showinfo("Error", "There was an error. Please try again.")
